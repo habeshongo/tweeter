28f9783ee3231cb1149b3b33e8141a2dca3539a2
Ashton Ferguson <doctorarferguson@gmail.com>
2023-04-06 23:31:42 -0300

    [Update]-Refactor character counter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   // --- our code goes here ---
-  $("#tweet-text").on("keyup", function () {
+  $("#tweet-text").on("input", function () {
     // console.log($(this).val());
     let charCount = $(this).val().length;
     let remainingCharCount = 140 - charCount;
     let counterElement = $(this).closest("form").find(".counter");
-    if (remainingCharCount < 0) {
+    if (remainingCharCount > 0 && remainingCharCount < 140) {
+      $(".counter").css("color", "black");
+    } else {
       $(".counter").css("color", "red");
     }
     counterElement.text(remainingCharCount);
